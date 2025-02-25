9e11bdf12d905bcc12a0f57e2eff4a7f5035323a
Charles AGBO <agbo.charles85@gmail.com>
2025-02-25 10:35:51 +0100

    Add endpoints for get all adventurers

--- back/src/main/java/com/ynov/capuches/opale/controllers/AdventurerController.java
+++ back/src/main/java/com/ynov/capuches/opale/controllers/AdventurerController.java
@@ -1,4 +1,29 @@
 package com.ynov.capuches.opale.controllers;
 
+import com.ynov.capuches.opale.entities.Adventurer;
+import com.ynov.capuches.opale.services.AdventurerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/adventurers")
 public class AdventurerController {
+
+    @Autowired
+    private AdventurerService adventurerService;
+
+    @GetMapping
+    public ResponseEntity<List<Adventurer>> getAllAdventurers() {
+        try {   
+            List<Adventurer> adventurers = adventurerService.getAllAdventurers();
+            return ResponseEntity.ok(adventurers);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
 }
--- back/src/main/java/com/ynov/capuches/opale/services/AdventurerService.java
+++ back/src/main/java/com/ynov/capuches/opale/services/AdventurerService.java
@@ -1,7 +1,18 @@
 package com.ynov.capuches.opale.services;
 
+import com.ynov.capuches.opale.entities.Adventurer;
+import com.ynov.capuches.opale.repositories.AdventurerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 @Service
 public class AdventurerService {
+
+    @Autowired
+    private AdventurerRepository adventurerRepository;
+
+    public List<Adventurer> getAllAdventurers() {
+        return adventurerRepository.findAll();
+    }
 }
