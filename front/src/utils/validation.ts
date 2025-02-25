4317884ddce576f4ca6ae03cd6615cd8a02cd31f
Alan <alan.jordy1999@gmail.com>
2025-02-25 15:40:33 +0100

    correction du conflit mal résolu

--- front/src/utils/validation.ts
+++ front/src/utils/validation.ts
@@ -2,11 +2,7 @@ import * as z from "zod";
 
 export const AdventurerFormSchema = z.object({
   name: z.string().min(1, "Le nom est requis"),
-<<<<<<< HEAD
   archetype: z.string().min(1, "La spécialité est requis"),
-=======
-  archetype: z.string().min(1, "L'archétype est requis"),
->>>>>>> 42f039e (resolution de conflit)
   experience: z.number().int().min(0, "L'expérience est requise"),
   daily_rate: z.number().int().min(0, "Le taux journalier est requis"),
 });
