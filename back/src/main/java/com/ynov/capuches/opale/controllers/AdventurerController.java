d28061222d82d247486fc54a2d7a1a70ebec2fd8
Gsorin <georgio.sorin@gmail.com>
2025-02-25 11:06:30 +0100

    feat(16): bug toEntity mapper

--- back/src/main/java/com/ynov/capuches/opale/controllers/AdventurerController.java
+++ back/src/main/java/com/ynov/capuches/opale/controllers/AdventurerController.java
@@ -1,4 +1,34 @@
 package com.ynov.capuches.opale.controllers;
 
-public class AdventurerController {
+
+import com.ynov.capuches.opale.entities.Adventurer;
+import com.ynov.capuches.opale.interfaces.AdventurerMapper;
+import com.ynov.capuches.opale.model.AdventurerDTO;
+import com.ynov.capuches.opale.openapi.api.AdventurerApiDelegate;
+import com.ynov.capuches.opale.services.AdventurerService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class AdventurerController implements AdventurerApiDelegate {
+    private final AdventurerService adventurerService;
+
+    public AdventurerController(AdventurerService adventurerService) {
+        this.adventurerService = adventurerService;
+    }
+
+    @Override
+    public ResponseEntity<AdventurerDTO> createAdventurer(AdventurerDTO adventurerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(adventurerService.createAdventurer(adventurerDTO));
+    }
+
+    @Override
+    public ResponseEntity<List<AdventurerDTO>> getAdventurers() {
+        return AdventurerApiDelegate.super.getAdventurers();
+    }
+
 }
---
+++ back/src/main/java/com/ynov/capuches/opale/interfaces/AdventurerMapper.java
@@ -0,0 +1,10 @@
+package com.ynov.capuches.opale.interfaces;
+import com.ynov.capuches.opale.entities.Adventurer;
+import com.ynov.capuches.opale.model.AdventurerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AdventurerMapper {
+    Adventurer toEntity(AdventurerDTO dto);
+    AdventurerDTO toDTO(Adventurer entity);
+}
