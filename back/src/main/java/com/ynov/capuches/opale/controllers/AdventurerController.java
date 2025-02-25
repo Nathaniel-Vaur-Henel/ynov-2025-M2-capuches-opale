3bacf89ee6644e4c82b6d1a58c29946e660e6312
GsorinDev <71591858+GsorinDev@users.noreply.github.com>
2025-02-25 14:34:19 +0100

    16 tech back création du profil daventurier (#46)
    * feat(16): generate base project + adventurer
    
    * feat(16): bug toEntity mapper
    
    * feat(16): entity generate
    
    * chore(#16): Remove warning on execution
    
    * feat(16): readd TestTest file java
    
    * feat(16): remove surfire and useless test
    
    ---------
    
    Co-authored-by: Nathan Schneider <nathan.schneider4505@gmail.com>

---
+++ back/src/main/java/com/ynov/capuches/opale/controllers/AdventurerController.java
@@ -0,0 +1,31 @@
+package com.ynov.capuches.opale.controllers;
+
+
+import com.ynov.capuches.opale.model.AdventurerDTO;
+import com.ynov.capuches.opale.openapi.api.AdventurerApiDelegate;
+import com.ynov.capuches.opale.services.AdventurerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class AdventurerController implements AdventurerApiDelegate {
+    private final AdventurerService adventurerService;
+
+    public AdventurerController(AdventurerService adventurerService) {
+        this.adventurerService = adventurerService;
+    }
+
+    @Override
+    public ResponseEntity<AdventurerDTO> createAdventurer(AdventurerDTO adventurerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(adventurerService.createAdventurer(adventurerDTO));
+    }
+
+    @Override
+    public ResponseEntity<List<AdventurerDTO>> getAdventurers() {
+        return AdventurerApiDelegate.super.getAdventurers();
+    }
+
+}
