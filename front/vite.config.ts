5509dfb4478447f1c8e09791caae0e4139c1a177
alvinlucas <84456349+alvinlucas@users.noreply.github.com>
2025-02-27 16:05:39 +0100

    (feat-front): Correction pour build du project

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [react()],
@@ -26,5 +26,4 @@ export default defineConfig({
 		setupFiles: "./src/setupTests.js",
 		globals: true,
 	},
-
 });
