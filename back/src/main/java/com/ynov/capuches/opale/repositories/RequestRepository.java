5053b633f29cb7fd9459c9b28880d2f6284219cf
Elouan <73824665+ByMSRT@users.noreply.github.com>
2025-02-27 16:40:34 +0100

    feat(RequestAPI) : Add "request" method PUT (#65)
    * feat(RequestAPI) : Add "request" method PUT
    
    * feat(RequestAPI) : Add "request" method PUT
    
    * feat(RequestAPI) : Add "request" method PUT
    
    * feat(RequestAPI) : Add "request" method PUT
    
    * feat(RequestAPI) : Add "request" method PUT
    
    * feat(RequestAPI) : Add "request" method PUT

--- back/src/main/java/com/ynov/capuches/opale/repositories/RequestRepository.java
+++ back/src/main/java/com/ynov/capuches/opale/repositories/RequestRepository.java
@@ -1,10 +1,14 @@
 package com.ynov.capuches.opale.repositories;
 
 import com.ynov.capuches.opale.entities.Request;
+import com.ynov.capuches.opale.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
+    Optional<Request> findByIdAndStatus(Long id, Status status);
 }
