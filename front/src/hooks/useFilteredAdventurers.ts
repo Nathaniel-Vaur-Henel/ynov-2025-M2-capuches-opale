ffb0ae1ab5c29e6af8b56fca4db6102826970ff6
Max <sauvagementvotre@protonmail.com>
2025-03-18 10:39:18 +0100

    Consultation et modification + correction du filtre archetype/spécialités

--- front/src/hooks/useFilteredAdventurers.ts
+++ front/src/hooks/useFilteredAdventurers.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { fetchData } from "../api/api.ts";
 import Adventurer from "../types/Adventurer.ts";
+import { Archetype } from "../utils/enum.ts";
 
 async function fetchAdventurers(): Promise<Adventurer[]> {
 	return fetchData<Adventurer[]>("/adventurer");
@@ -10,13 +11,9 @@ async function fetchAdventurers(): Promise<Adventurer[]> {
 export function useFilteredAdventurers() {
 	const [searchParams] = useSearchParams();
 	const searchTerm = searchParams.get("search") || "";
-	const selectedArchetype = searchParams.get("archetype") || "";
-	const sortField = searchParams.get("sort") as
-		| "experience"
-		| "dailyRate"
-		| undefined;
-	const sortDirection =
-		(searchParams.get("direction") as "asc" | "desc") || "desc";
+	const selectedArchetype = searchParams.get("archetype") as Archetype | "";
+	const sortField = searchParams.get("sort") as "experience" | "dailyRate" | undefined;
+	const sortDirection = (searchParams.get("direction") as "asc" | "desc") || "desc";
 
 	const query = useQuery({
 		queryKey: ["adventurers"],
@@ -31,6 +28,7 @@ export function useFilteredAdventurers() {
 						  adv.archetype.toLowerCase().includes(searchTerm.toLowerCase())
 						: true;
 
+					// Vérification avec l'Enum Archetype
 					const matchesArchetype = selectedArchetype
 						? adv.archetype === selectedArchetype
 						: true;
@@ -51,9 +49,8 @@ export function useFilteredAdventurers() {
 				})
 		: [];
 
-	const uniqueArchetypes = query.data
-		? Array.from(new Set(query.data.map((adv) => adv.archetype)))
-		: [];
+	// Générer la liste des archétypes en utilisant l'Enum Archetype
+	const uniqueArchetypes = Object.values(Archetype);
 
 	return {
 		adventurers: filteredAdventurers,
@@ -61,5 +58,3 @@ export function useFilteredAdventurers() {
 		...query,
 	};
 }
-
-export type { Adventurer };
